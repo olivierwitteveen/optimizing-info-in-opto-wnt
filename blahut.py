8c800343a784d65cf365da5ae278d6dcf89558e3
Olivier <74541441+olivierwitteveen@users.noreply.github.com>
2026-02-12 11:32:56 +0100

    All figure code, and a self-contained BA example.

--- blahut.py
+++ blahut.py
@@ -1,59 +1,57 @@
 import numpy as np
+from numba import njit, prange
 
-def blahut_update(xvec, yvec, p_x, p_y_given_x, discrete_x=False):
+@njit
+def blahut_update(p_x, p_y_given_x, eps=1e-20):
     """
-    Blahut algorithm for computing the mutual information between discrete or continuous variable X and continuous variable Y.
+    Blahut algorithm for computing the mutual information between variable X and variable Y.
     The function takes as input:
-    - xvec: the values of the random variable X (Nx1 array)
-    - yvec: the values of the random variable Y (Mx1 array)
-    - p_x: the probability mass function of X (Nx1 array)
+    - xvec: the values of the random variable X (N-dim array)
+    - yvec: the values of the random variable Y (M-dim array)
+    - p_x: the probability mass function of X (N-dim array)
     - p_y_given_x: the conditional probability mass function of Y given X (MxN array)
-    - discrete_x: boolean indicating whether X is discrete or continuous (bool)
     The function returns as output:
     - I_L: the lower bound to the mutual information (scalar)
     - I_U: the upper bound to the mutual information (scalar)
-    - f_kl_new: updated Bayes risk (Nx1 array)
-    - p_x_new: updated probability mass function of X (Nx1 array)
+    - f_kl_new: updated Bayes risk (N-dim array)
+    - p_y_new: updated probability mass function of Y (M-dim array)
+    - p_x_new: updated probability mass function of X (N-dim array)
     """
-    # discretisation steps
-    if not discrete_x:
-        dx = xvec[1] - xvec[0] # scalar
-    dy = yvec[1] - yvec[0] # scalar
+    N = len(p_x)
+    M = len(p_y_given_x)
 
-    # output distribution
-    p_y = np.matmul(p_y_given_x, p_x) # Mx1 array
-    p_y /= np.sum(p_y*dy)
-    p_y.reshape(-1,1)
+    p_y = np.zeros(M) # output dist
+    f_kl = np.zeros(N) # Bayes risk
+    p_x_new = np.zeros(N) # updated input dist
+    for a in range(N):
+        for b in range(M):
+            p_y[b] += p_y_given_x[b,a]*p_x[a]
+            
+    for a in range(N):
+        for b in range(M):  
+            f_kl[a] += p_y_given_x[b,a]*np.log((p_y_given_x[b,a]+eps)/(p_y[b]+eps))
+        p_x_new[a] = p_x[a]*np.exp(f_kl[a]) 
 
-    # Bayes risk
-    f_kl = np.sum(p_y_given_x*np.log((p_y_given_x+1e-10)/(p_y+1e-10)), axis=0)*dy 
-    f_kl = f_kl.reshape(-1,1) # Nx1 array
+    p_x_new /= p_x_new.sum() # normalize
 
-    # update prior
-    p_x_new = p_x*np.exp(f_kl).copy() # Nx1 array
-    if not discrete_x:
-        p_x_new /= np.sum(p_x_new*dx)
-    if discrete_x:
-        p_x_new /= np.sum(p_x_new)
+    p_y_new = np.zeros(M) # updated output dist
+    f_kl_new = np.zeros(N) # updated Bayes risk
+    I_L = 0 # lower bound to capacity
+    for a in range(N):
+        for b in range(M):
+            p_y_new[b] += p_y_given_x[b,a]*p_x_new[a]
+    
+    I_U = 0 # upper bound to capacity
+    for a in range(N):
+        for b in range(M):
+            f_kl_new[a] += p_y_given_x[b,a]*np.log((p_y_given_x[b,a]+eps)/(p_y_new[b]+eps))
+        I_L += p_x_new[a]*f_kl_new[a]
+        if f_kl_new[a] > I_U:
+            I_U = f_kl_new[a]
 
-    # new output distribution
-    p_y_new = np.matmul(p_y_given_x, p_x_new) # Mx1 array
-    p_y_new /= np.sum(p_y_new*dy)
-    p_y_new.reshape(-1,1)
-
-    f_kl_new = np.sum(p_y_given_x*np.log((p_y_given_x+1e-10)/(p_y_new+1e-10)), axis=0)*dy
-    f_kl_new = f_kl_new.reshape(-1,1) # Nx1 array
-
-    # lower and upper bounds to channel capacity
-    if not discrete_x:
-        I_L = (np.sum(p_x_new*f_kl_new)*dx)[0]
-    if discrete_x:
-        I_L = (np.sum(p_x_new*f_kl_new))
-
-    I_U = np.max(f_kl_new)
     return I_L, I_U, f_kl_new, p_x_new
 
-def weight_step(convergence_goal, max_iter, yvec, pos, weightvec, p_y_given_x):
+def weight_step(convergence_goal, max_iter, weightvec, p_y_given_x):
     """
     When starting out with a discrete prior with K atoms, this function will run a Blahut algorithm until a chosen level of convergence to set the weights of the atoms. 
     The function takes as input:
@@ -69,13 +67,21 @@ def weight_step(convergence_goal, max_iter, yvec, pos, weightvec, p_y_given_x):
     I_L, I_U = 0, 1
     tau = 0
     while I_U - I_L > convergence_goal and tau < max_iter:
-        #I_L, I_U, weightvec = discrete_blahut_update(yvec, weightvec, p_y_given_x)
-        I_L, I_U, f_kl_new, weightvec = blahut_update(pos, yvec, weightvec, p_y_given_x, discrete_x=True)
+        I_L, I_U, f_kl_new, weightvec = blahut_update(weightvec, p_y_given_x)
         tau += 1
     err = I_U - I_L
     return err, weightvec
 
 def pos_step(yvec, weightvec, p_y_given_x, d_pyx_dx):
+    """
+    When starting out with a discrete prior with K atoms, this function will return the gradients of the positions of the atoms.
+    - yvec: the values of the random variable Y (Mx1 array)
+    - weightvec: the weight of each atom of X (Kx1 array)
+    - p_y_given_x: the conditional probability mass function of Y given X (MxK array)
+    The function returns as output:
+    - pos_grad: the gradient of the positions of the atoms (Kx1 array)
+    """
+
     # discretisation steps
     dy = yvec[1] - yvec[0]
 
@@ -88,43 +94,3 @@ def pos_step(yvec, weightvec, p_y_given_x, d_pyx_dx):
     pos_grad = pos_grad*weightvec
 
     return pos_grad
-
-def blahut_update_disc(p_x, p_y_given_x):
-    """
-    Blahut algorithm for computing the mutual information between discrete variable X and discrete variable Y.
-    The function takes as input:
-    - p_x: the probability mass function of X (Nx1 array)
-    - p_y_given_x: the conditional probability mass function of Y given X (MxN array)
-    The function returns as output:
-    - I_L: the lower bound to the mutual information (scalar)
-    - I_U: the upper bound to the mutual information (scalar)
-    - f_kl_new: updated Bayes risk (Nx1 array)
-    - p_x_new: updated probability mass function of X (Nx1 array)
-    """
-
-    # output distribution
-    p_y = np.matmul(p_y_given_x, p_x) # Mx1 array
-    p_y /= np.sum(p_y)
-    p_y.reshape(-1,1)
-
-    # Bayes risk
-    f_kl = np.sum(p_y_given_x*np.log((p_y_given_x+1e-10)/(p_y+1e-10)), axis=0) 
-    f_kl = f_kl.reshape(-1,1) # Nx1 array
-
-    # update prior
-    p_x_new = p_x*np.exp(f_kl).copy() # Nx1 array
-    p_x_new /= np.sum(p_x_new)
-
-    # new output distribution
-    p_y_new = np.matmul(p_y_given_x, p_x_new) # Mx1 array
-    p_y_new /= np.sum(p_y_new)
-    p_y_new.reshape(-1,1)
-
-    f_kl_new = np.sum(p_y_given_x*np.log((p_y_given_x+1e-10)/(p_y_new+1e-10)), axis=0)
-    f_kl_new = f_kl_new.reshape(-1,1) # Nx1 array
-
-    # lower and upper bounds to channel capacity
-    I_L = (np.sum(p_x_new*f_kl_new))
-
-    I_U = np.max(f_kl_new)
-    return I_L, I_U, f_kl_new, p_x_new
